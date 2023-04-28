78981a9bf2c44033b45337405181d6c15b726aa2
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-04-28 09:50:21 -0500

    adding conditional for more than 1 warning label

--- Medications/index.js
+++ Medications/index.js
@@ -12,7 +12,6 @@ const handlingInstructions = (orders, medications) => {
 
         // map through instructions
         let newInstruction = instructions.map((instruction)=>(instruction))
-        console.log(newInstruction.indexOf('No Special Instructions'));
 
     // list of orders(separated by semicolon): ID, Medication Name 
         // Example: Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA
@@ -70,42 +69,53 @@ const handlingInstructions = (orders, medications) => {
             // push new new object into empty array
             medicationList.push(newMedication);
 
-           // console.log(newMedication.label);
             const instructionNumber = newMedication.label.split(',');
-            //console.log(instructionNumber);
             
-
             // output = list of orders
             // Example Format: [OrderID]:[Medication Name]:[WARNING-Instructions (comma separated with whitespace)]
             // Example Output: Rx1:MedicationX:WARNING-Do Not Shake, Must Refrigerate
             
             // conditional statement to check if medications match order 
-            if (newOrders.medication === newMedication.medicationName) {
-                //console.log(newOrders.medication, newMedication.medicationName);
-                //console.log(newMedication.label)
+            if (newOrders.medication === newMedication.medicationName ) {
 
                 // get warning label
                 let warningNumber = newMedication.label;
-                let warningLabel = newInstruction[warningNumber];
-                console.log(warningLabel);
+                
+                // conditional statement for 2 warning labels
+                if (warningNumber.length >= 2 ) {
+                    console.log(warningNumber);
+                    let warningLabel = [];
 
-                    const mergedOrder = [
+                    // split string into subarray
+                    let newNumber = warningNumber.split(',');
+                    console.log(newNumber);
+                   
+                    // loop through new array 
+                    const element = newNumber.forEach(element => 
+                    // push corresponding instructions to empty array
+                    warningLabel.push(newInstruction[element]))
+
+                    console.log(warningLabel);  
+                    
+                     const mergedOrder = [
                         newOrders.ID,
                         newOrders.medication,
-                        warningLabel,
-                        warningNumber
+                        'WARNING',
+                        warningLabel
                         ]
                     
                     console.log(mergedOrder);
 
                 // push merged order to empty final array 
-                finishedOrders.push(mergedOrder); 
-            }       
+                finishedOrders.push(mergedOrder);
+                }   
+            } 
         }  
     } 
-   console.log(finishedOrders);      
+   console.log(finishedOrders); 
+   //console.log(finishedOrders.toString());   
 }   
 
-handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ', 'MedicationA:1,2,4;MedicationX:1,2;MedicationY:0;MedicationZ:3');
+handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA', 'MedicationA:1,2,3;MedicationX:1,2;MedicationY:0;MedicationZ:3');
 
 //module.exports = handlingInstructions;
