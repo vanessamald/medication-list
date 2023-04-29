59020b2d6fa1b9477cecb64229906b5f0b97aff1
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-04-29 09:43:16 -0500

    accounting for edge case where label number does not exist by filtering out numbers greater than 3

--- Medications/index.js
+++ Medications/index.js
@@ -1,41 +1,45 @@
 // accept two strings
 const handlingInstructions = (orders, medications) => {
+    //empty array holding final orders
     let finishedOrders = []; 
 
-      // instructions: “Do Not Shake”, Keep Refrigerated”, “Keep Away from Heat” (include "!!!" for all 3 )
-      let instructions = [
-        'No Special Instructions',
-        'Do Not Shake',
-        'Keep Refrigerated',
-        'Keep Away from Heat',
-        ]
+    // array of instructions
+    let instructions = [
+                        'No Special Instructions',
+                        'Do Not Shake',
+                        'Keep Refrigerated',
+                        'Keep Away from Heat',
+                        ]
 
-        // map through instructions
-        let newInstruction = instructions.map((instruction)=>(instruction))
-
-    // list of orders(separated by semicolon): ID, Medication Name 
-        // Example: Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA
+    // map through instructions
+    let newInstruction = instructions.map((instruction)=>(instruction))
         
-        // empty array that will hold list of orders 
-        const orderList = [];
+    // empty array that will hold list of orders 
+    const orderList = [];
 
-        // split string by semicolon
-        const entries = orders.split(';');
+    // split 'order' string by semicolon into an array
+    const entries = orders.split(';');
         //console.log(entries);
 
-        // loop through new array
-        for (let i=0; i< entries.length; i++) {
+    // empty array for medications
+    const medicationList = [];
+      
+    const meds = medications.split(';');
+
+    // List of Orders Example: Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA
+
+    // loop through new array
+    for (let i=0; i< entries.length; i++) {
             
         // split new array by colon
         let entry = entries[i].split(':');
-
 
         // deconstruct array
         const [ orderID, medicationName ] = entry;
         // create object with ID, medicationName
         let newOrders = {
-                            ID: orderID, 
-                            medication: medicationName
+                        ID: orderID, 
+                        medication: medicationName
                         }
         
         // push object into empty array
@@ -44,17 +48,17 @@ const handlingInstructions = (orders, medications) => {
         // list of medications(separated by semicolon): Medication Name, Special Handling Instructions
         //Example: MedicationA:1,2,4;MedicationX:1,2;MedicationY:0;MedicationZ:4;
 
+        /*
         // empty array for medications
         const medicationList = [];
-        //console.log(medicationList)
         
         // sort through medications and look for duplicates
         let sorted_meds = medications.split(';').sort();  
         //console.log(sorted_meds);          
 
-
         const meds = medications.split(';');
-        
+        */
+       
         // loop through medications
         for (let j=0; j< meds.length; j++ ) {
 
@@ -76,48 +80,59 @@ const handlingInstructions = (orders, medications) => {
                                 label: newLabel
                                 };
 
-                                console.log(newMedication)
+                                //console.log(newMedication)
+
             // push new new object into empty array
             medicationList.push(newMedication);
 
-            // output = list of orders
+            // List of Final Orders 
             // Example Format: [OrderID]:[Medication Name]:[WARNING-Instructions (comma separated with whitespace)]
             // Example Output: Rx1:MedicationX:WARNING-Do Not Shake, Must Refrigerate
             
             // conditional statement to check if medications match order 
             if (newOrders.medication === newMedication.medicationName ) {
 
-                // get warning label
+                // get warning number 
                 let warningNumber = newMedication.label;
+                    let warningArray = warningNumber.split(',')
+                    // map through warning number
+                    let newWarningNumber = warningArray.map((number)=>(number)) 
+                    console.log(newWarningNumber);
+                    // filter out numbers greater than 4
+                    let newNumbers = newWarningNumber.filter((element) => element < 4);
+                    console.log(newNumbers.length, newNumbers);
 
-                // conditional for no 'WARNING' label
-                if  (warningNumber === '0' )  {
-                    
-                    let warningLabel = newInstruction[warningNumber];
-                
-                    
-                    // add WARNING to label
+                // get corresponding label
+                let warningLabel = newInstruction[warningNumber];
+                    console.log(warningLabel);
+
+                // if no 'WARNING' needed on label
+                if  (warningNumber === '0' || newNumbers.length === 0 )  {
+                     
+                    // merge order id and medication name into new array
                     const mergedOrder = [
-                                        [newOrders.ID],
-                                        [newOrders.medication]  
+                                        newOrders.ID,
+                                        newOrders.medication  
                                         ]
     
                     // push merged order to empty final array 
                     finishedOrders.push(mergedOrder); 
                 }
-                
-                 // split string into subarray
-                 let newNumber = warningNumber.split(',')
-                    //console.log(newNumber);
-                   
 
-                // conditional statement for 2+ warning labels
-                if (newNumber.length >= 1 && warningNumber !== '0' ) {
-                    //console.log(warningNumber.length);
+               /*
+                // split warning number into a subarray
+                let newNumber = warningNumber.split(',')
+                    console.log(newNumber);*/
+               
+                // if warning label does not equal 0 
+                if (newNumbers.length >= 1 && warningNumber !== '0' ) {
+                   
+                    
+                    // array holding multiple Warnings
                     let warningLabel = [];
 
-                    // loop through new array 
-                    const element = newNumber.forEach(element => 
+                    // loop through filtered Warning Numbers  
+                    const element = newNumbers.forEach(element => 
                     // push corresponding instructions to empty array
                     warningLabel.push(newInstruction[element]))
                     //console.log(warningLabel);  
@@ -129,13 +144,9 @@ const handlingInstructions = (orders, medications) => {
                         ]
 
                      // if there are 3 warning labels  
-                     if (newNumber.length === 3 ) {
-                        // testing edge case if warning label does not exist 
-                        //console.log(warningLabel.includes(undefined));
-
-                        //console.log('!!!')
-                        //console.log(newNumber.length);
-                        //console.log(newOrders.medication);
+                     if (newNumbers.length === 3 ) {
+                        // TESTING edge case if warning label does not exist 
+                        console.log(warningLabel.includes(undefined));
 
                         // splice array, add 'WARNING' WITH '!!!' for 3 labels
                         mergedOrder.splice(2, 0, 'WARNING!!!');
@@ -156,8 +167,6 @@ const handlingInstructions = (orders, medications) => {
    console.log(finishedOrders);  
 }  
 
-
-
-handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA', 'MedicationA:1,2,3;MedicationX:1,1,2;MedicationY:0;MedicationZ:3');
+handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA', 'MedicationA:1,2,4;MedicationX:1,1;MedicationY:0;MedicationZ:4');
 
 //module.exports = handlingInstructions;
