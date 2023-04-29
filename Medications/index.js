4b64112aefc70b95cf05790c6233c847bc966a4e
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-04-28 19:57:43 -0500

    adding Set object to remove duplicate labels in medication order edge case

--- Medications/index.js
+++ Medications/index.js
@@ -46,40 +46,40 @@ const handlingInstructions = (orders, medications) => {
 
         // empty array for medications
         const medicationList = [];
-        console.log(medicationList)
+        //console.log(medicationList)
         
         // sort through medications and look for duplicates
         let sorted_meds = medications.split(';').sort();  
-        console.log(sorted_meds);           
+        //console.log(sorted_meds);          
 
 
         const meds = medications.split(';');
-        //console.log(meds);
         
         // loop through medications
         for (let j=0; j< meds.length; j++ ) {
-            // check if there is a duplicate
-            if(sorted_meds[j + 1] == sorted_meds[j]) {
 
-            }
             // split new array by colon
             let med = meds[j].split(':');
-            //console.log(med);
-
+            
             // deconstruct array
             const [ name, label ] = med;
+            // split label into array
+            const labelArray =  label.split(',');
+
+                  // delete duplicate labels 
+                  let uniqueLabel = [...new Set(labelArray)];
+                  let newLabel = uniqueLabel.toString();
     
             // new medication object
             let newMedication = { 
                                 medicationName: name, 
-                                label: label
+                                label: newLabel
                                 };
-    
+
+                                console.log(newMedication)
             // push new new object into empty array
             medicationList.push(newMedication);
 
-            const instructionNumber = newMedication.label.split(',');
-            
             // output = list of orders
             // Example Format: [OrderID]:[Medication Name]:[WARNING-Instructions (comma separated with whitespace)]
             // Example Output: Rx1:MedicationX:WARNING-Do Not Shake, Must Refrigerate
@@ -92,8 +92,7 @@ const handlingInstructions = (orders, medications) => {
 
                 // conditional for no 'WARNING' label
                 if  (warningNumber === '0' )  {
-                    console.log(warningNumber);
-                    console.log(newMedication.label);
+                    
                     let warningLabel = newInstruction[warningNumber];
                 
                     
@@ -106,10 +105,11 @@ const handlingInstructions = (orders, medications) => {
                     // push merged order to empty final array 
                     finishedOrders.push(mergedOrder); 
                 }
-                //console.log(warningNumber.split(','));
+                
                  // split string into subarray
-                 let newNumber = warningNumber.split(',');
-                    console.log(newNumber, newNumber.length);
+                 let newNumber = warningNumber.split(',')
+                    //console.log(newNumber);
+                   
 
                 // conditional statement for 2+ warning labels
                 if (newNumber.length >= 1 && warningNumber !== '0' ) {
@@ -120,24 +120,22 @@ const handlingInstructions = (orders, medications) => {
                     const element = newNumber.forEach(element => 
                     // push corresponding instructions to empty array
                     warningLabel.push(newInstruction[element]))
-                    console.log(warningLabel);  
+                    //console.log(warningLabel);  
                     
                      const mergedOrder = [
                         newOrders.ID,
                         newOrders.medication,
                         warningLabel
                         ]
-                    
-                    console.log(mergedOrder);
 
                      // if there are 3 warning labels  
-                     if (newNumber.length === 3 && warningLabel.includes(undefined)  === false ) {
+                     if (newNumber.length === 3 ) {
                         // testing edge case if warning label does not exist 
-                        console.log(warningLabel.includes(undefined));
+                        //console.log(warningLabel.includes(undefined));
 
-                        console.log('!!!')
-                        console.log(newNumber.length);
-                        console.log(newOrders.medication);
+                        //console.log('!!!')
+                        //console.log(newNumber.length);
+                        //console.log(newOrders.medication);
 
                         // splice array, add 'WARNING' WITH '!!!' for 3 labels
                         mergedOrder.splice(2, 0, 'WARNING!!!');
@@ -149,15 +147,17 @@ const handlingInstructions = (orders, medications) => {
                     mergedOrder.splice(2, 0, 'WARNING');
 
                     // push merged order to empty final array 
-                    finishedOrders.push(mergedOrder.join(':'));
+                    finishedOrders.push(mergedOrder);
                     }
                 }  
             } 
         }  
     } 
    console.log(finishedOrders);  
-}   
+}  
 
-handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA', 'MedicationA:1,2,4;MedicationX:1,1,2;MedicationY:0;MedicationZ:3');
+
+
+handlingInstructions('Rx1:MedicationX;Rx2:MedicationY;Rx3:MedicationZ;Rx4:MedicationA', 'MedicationA:1,2,3;MedicationX:1,1,2;MedicationY:0;MedicationZ:3');
 
 //module.exports = handlingInstructions;
